496353b6a988f3feec0683d74560a0f85d5e68a5
aklein13 <arek.klein@gmail.com>
2017-11-06 11:17:29 +0100

    add all drinks + test for it (names are unique)

--- src/main/java/com/example/jdbcdemo/service/DrinkManager.java
+++ src/main/java/com/example/jdbcdemo/service/DrinkManager.java
@@ -1,5 +1,6 @@
 package com.example.jdbcdemo.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.example.jdbcdemo.domain.Drink;
@@ -8,5 +9,5 @@ public interface DrinkManager {
 	
 	public int addDrink(Drink drink);
 	public List<Drink> getAllDrinks();
-
+	public void addAllDrinks(List<Drink> list);
 }
--- src/test/java/com/example/jdbcdemo/service/DrinkManagerTest.java
+++ src/test/java/com/example/jdbcdemo/service/DrinkManagerTest.java
@@ -2,10 +2,12 @@ package com.example.jdbcdemo.service;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.jdbcdemo.domain.Drink;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class DrinkManagerTest {
 
@@ -13,8 +15,12 @@ public class DrinkManagerTest {
 	private DrinkManagerJDBC drinkManager = new DrinkManagerJDBC();
 	
 	private final static String NAME_1 = "Cola";
+	private final static String NAME_2 = "Fanta";
+	private final static String NAME_3 = "Sprite";
 	private final static double PRICE_1 = 5.5;
-	
+	private final static double PRICE_2 = 4.5;
+	private final static double PRICE_3 = 3.5;
+
 	@Test
 	public void checkConnection(){
 		assertNotNull(drinkManager.getConnection());
@@ -34,4 +40,31 @@ public class DrinkManagerTest {
 		
 	}
 
+	@Test
+	public void checkAddingListSuccess(){
+		drinkManager.clearDrinks();
+		List<Drink> listToAdd = new ArrayList<>();
+		Drink drink1 = new Drink(NAME_1, PRICE_1);
+		Drink drink2 = new Drink(NAME_1, PRICE_1);
+		Drink drink3 = new Drink(NAME_3, PRICE_3);
+		listToAdd.add(drink1);
+		listToAdd.add(drink2);
+		listToAdd.add(drink3);
+		drinkManager.addAllDrinks(listToAdd);
+		List<Drink> drinks = drinkManager.getAllDrinks();
+		System.out.println(drinks.size());
+		assertThat(drinks.size(), either(is(0)).or(is(3)));
+		drinkManager.clearDrinks();
+		listToAdd = new ArrayList<>();
+		drink1 = new Drink(NAME_1, PRICE_1);
+		drink2 = new Drink(NAME_2, PRICE_2);
+		drink3 = new Drink(NAME_3, PRICE_3);
+		listToAdd.add(drink1);
+		listToAdd.add(drink2);
+		listToAdd.add(drink3);
+		drinkManager.addAllDrinks(listToAdd);
+		drinks = drinkManager.getAllDrinks();
+		System.out.println(drinks.size());
+		assertThat(drinks.size(), either(is(0)).or(is(3)));
+	}
 }
